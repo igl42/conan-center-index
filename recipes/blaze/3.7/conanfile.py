515eab85d56c5ca3c24dad1a5e34158a8d51bc87
Klaus Iglberger <klaus.iglberger@gmail.com>
2020-02-28 06:25:31 +0100

    [blaze] Add recipes for Blaze 3.4 to 3.7

---
+++ recipes/blaze/3.7/conanfile.py
@@ -0,0 +1,22 @@
+import os
+from conans import ConanFile
+from conans.tools import Version
+
+
+class BlazeConan(ConanFile):
+    name = "blaze"
+    version = "3.7"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://bitbucket.org/blaze-lib/blaze"
+    description = "open-source, high-performance C++ math library for dense and sparse arithmetic"
+    topics = ("conan", "blaze", "math", "algebra", "linear algebra", "high-performance")
+    license = "New (Revised) BSD license"
+
+    no_copy_source = True
+
+    def source(self):
+        self.run("git clone https://bitbucket.org/blaze-lib/blaze")
+        self.run("cd blaze && git checkout v%s" % self.version)
+
+    def package(self):
+        self.copy(pattern="*.h", src="blaze", dst="include")
